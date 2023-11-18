4df77f351292690a9a0894783da1867ec2659bcd
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-11-18 00:06:31 +0000

    Deploying to gh-pages from @ JLuisNG/goit-js-hw-10@cf13d6a3693a570aa82e58639e172efdf5908e69 🚀

--- index.2e46ff02.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".gallery");e.addEventListener("mouseenter",(()=>{let t=Math.random()*e.offsetWidth,r=Math.random()*e.offsetHeight;e.style.setProperty("--ledX",t+"px"),e.style.setProperty("--ledY",r+"px")}));
-//# sourceMappingURL=index.2e46ff02.js.map
--- index.6abd164a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".gallery");e.addEventListener("mouseenter",(function(){var t=Math.random()*e.offsetWidth,r=Math.random()*e.offsetHeight;e.style.setProperty("--ledX",t+"px"),e.style.setProperty("--ledY",r+"px")}))}();
-//# sourceMappingURL=index.6abd164a.js.map
